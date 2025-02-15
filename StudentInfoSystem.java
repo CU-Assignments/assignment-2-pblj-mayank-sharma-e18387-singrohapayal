675bb4bf51728eca85efebeb1cd72a3429a68e1f
singrohapayal <singrohapayal@gmail.com>
2025-02-16 02:09:04 +0530

    Create StudentInfoSystem.java

---
+++ StudentInfoSystem.java
@@ -0,0 +1,65 @@
+abstract class Person {
+    protected String name;
+    protected int age;
+
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Abstract method to display details
+    public abstract void displayDetails();
+}
+
+// Derived class for Student
+class Student extends Person {
+    private int rollNumber;
+
+    public Student(String name, int age, int rollNumber) {
+        super(name, age);
+        this.rollNumber = rollNumber;
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Student Details:");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Roll Number: " + rollNumber);
+    }
+}
+
+// Derived class for Teacher
+class Teacher extends Person {
+    private String subject;
+
+    public Teacher(String name, int age, String subject) {
+        super(name, age);
+        this.subject = subject;
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Teacher Details:");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Subject: " + subject);
+    }
+}
+
+// Main class to test the system
+public class StudentInformationSystem {
+    public static void main(String[] args) {
+        // Creating Student object
+        Student student = new Student("Alice", 20, 101);
+        
+        // Creating Teacher object
+        Teacher teacher = new Teacher("Mr. Smith", 40, "Mathematics");
+        
+        // Displaying details
+        student.displayDetails();
+        System.out.println();
+        teacher.displayDetails();
+    }
+}
